7abfb3fc9e14fefd77fbf780193f85acd3148765
Vincent Roth <vincent@placeme.io>
2023-09-26 16:14:50 +0200

    Observable - désouscription

--- src/app/animal/animal-detail/animal-detail.component.ts
+++ src/app/animal/animal-detail/animal-detail.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Animal } from '../../shared/api/animal';
 import { AnimalService } from '../../shared/api/animal.service';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-animal-detail',
   templateUrl: './animal-detail.component.html',
   styleUrls: ['./animal-detail.component.scss'],
 })
-export class AnimalDetailComponent implements OnInit {
+export class AnimalDetailComponent implements OnInit, OnDestroy {
   animal?: Animal;
+  private subscription: Subscription;
 
   constructor(
     private animalService: AnimalService,
@@ -18,7 +19,7 @@ export class AnimalDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activedRoute.paramMap
+    this.subscription = this.activedRoute.paramMap
       .pipe(
         switchMap((paramMap) =>
           this.animalService.get(Number(paramMap.get('id')))
@@ -27,5 +28,9 @@ export class AnimalDetailComponent implements OnInit {
       .subscribe((data) => {
         this.animal = data;
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
