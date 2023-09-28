99c7143d7f2b222fc68986a7201af030eeb3d148
Vincent Roth <vincent@placeme.io>
2023-09-28 11:54:57 +0200

    Test unitaire - HttpClient et surcharge de service

--- src/app/animal/animal-detail/animal-detail.component.spec.ts
+++ src/app/animal/animal-detail/animal-detail.component.spec.ts
@@ -1,19 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnimalDetailComponent } from './animal-detail.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SharedModule } from '../../shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Animal } from '../../shared/api/animal';
 
 describe('AnimalDetailComponent', () => {
   let component: AnimalDetailComponent;
   let fixture: ComponentFixture<AnimalDetailComponent>;
+  let httpCtrl: HttpTestingController;
 
   beforeEach(() => {
+    const paramMap = new Map();
+    paramMap.set('id', 1);
+
     TestBed.configureTestingModule({
       declarations: [AnimalDetailComponent],
       imports: [HttpClientTestingModule, RouterTestingModule, SharedModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(paramMap),
+            snapshot: {
+              paramMap, // paramMap: paramMap
+            },
+          },
+        },
+      ],
     });
+
+    httpCtrl = TestBed.inject(HttpTestingController);
+
     fixture = TestBed.createComponent(AnimalDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +45,17 @@ describe('AnimalDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should request correct animal for a given ID', () => {
+    const requestCtrl = httpCtrl.expectOne('/api/animals/1');
+    const animal: Animal = {
+      id: 1,
+      name: 'Name',
+      species: 'Species',
+    };
+    requestCtrl.flush(animal);
+
+    expect(component.animal?.id).toBe(1);
   });
 });
--- src/app/animal/animal-list/animal-list.component.spec.ts
+++ src/app/animal/animal-list/animal-list.component.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnimalListComponent } from './animal-list.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AnimalItemComponent } from '../animal-item/animal-item.component';
+import { Animal } from '../../shared/api/animal';
 
 describe('AnimalListComponent', () => {
   let component: AnimalListComponent;
   let fixture: ComponentFixture<AnimalListComponent>;
+  let httpCtrl: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AnimalListComponent, AnimalItemComponent],
       imports: [HttpClientTestingModule, RouterTestingModule],
     });
+    httpCtrl = TestBed.inject(HttpTestingController);
+
     fixture = TestBed.createComponent(AnimalListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +28,28 @@ describe('AnimalListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a list of animals in template', () => {
+    const requestCtrl = httpCtrl.expectOne('/api/animals');
+    expect(requestCtrl.request.method).toBe('GET');
+
+    expect(component.animals).toBeUndefined();
+
+    const animals: Animal[] = [
+      { id: 1, name: 'Name1', species: 'Species1' },
+      { id: 2, name: 'Name2', species: 'Species2' },
+      { id: 3, name: 'Name3', species: 'Species3' },
+    ];
+    requestCtrl.flush(animals);
+
+    expect(component.animals?.length).toBe(3);
+
+    httpCtrl.verify();
+
+    // Template
+    fixture.detectChanges();
+    const template: HTMLElement = fixture.nativeElement;
+    expect(template.querySelectorAll('li')?.length).toBe(3);
   });
 });
