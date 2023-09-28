7e5fe93f7d11eb5b8cd9ba061859b702e874a06f
Vincent Roth <vincent@placeme.io>
2023-09-28 11:31:47 +0200

    Test unitaire - pipe

--- src/app/shared/phone.pipe.spec.ts
+++ src/app/shared/phone.pipe.spec.ts
@@ -1,8 +1,43 @@
 import { PhonePipe } from './phone.pipe';
 
 describe('PhonePipe', () => {
+  let pipe: PhonePipe;
+
+  beforeEach(() => {
+    pipe = new PhonePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new PhonePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform value with default separator', () => {
+    const input = '01.23.45.67.89';
+    const expectedOuput = '01 23 45 67 89';
+    expect(pipe.transform(input)).toBe(expectedOuput);
+  });
+
+  it('should transform value with defined separator', () => {
+    const input = '01.23.45.67.89';
+    const expectedOuput = '01-23-45-67-89';
+    expect(pipe.transform(input, '-')).toBe(expectedOuput);
+  });
+
+  it('should not transform value with nothing to replace', () => {
+    const input = '0123456789';
+    expect(pipe.transform(input)).toBe(input);
+  });
+
+  it('should handle non string value', () => {
+    const input: any = 123456789;
+    expect(pipe.transform(input)).toBe(input);
+  });
+
+  it('should handle undefined value', () => {
+    expect(pipe.transform(undefined)).toBeUndefined();
+  });
+
+  it('should handle null value', () => {
+    expect(pipe.transform(null)).toBeNull();
   });
 });
--- src/app/shared/phone.pipe.ts
+++ src/app/shared/phone.pipe.ts
@@ -5,6 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PhonePipe implements PipeTransform {
   transform(value: string, separator = ' '): string {
+    if (typeof value !== 'string') {
+      return value;
+    }
     return value.replaceAll('.', separator);
   }
 }
